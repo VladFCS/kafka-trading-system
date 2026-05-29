756e643efaa33a10f697a5e534d4160e310de2a9
Vladyslav Fedirko <vladteua@gmail.com>
2026-05-29 12:42:32 +0300

    feat: add schemas for order service

--- db/queries/order.sql
+++ db/queries/order.sql
@@ -1,1 +1,47 @@
+-- name: CreateOrder :one
+INSERT INTO orders (
+  id,
+  customer_id,
+  symbol,
+  side,
+  price,
+  quantity,
+  remaining_quantity,
+  status,
+  idempotency_key,
+  canceled_at,
+  created_at,
+  updated_at
+) VALUES (
+  sqlc.arg(id),
+  sqlc.arg(customer_id),
+  sqlc.arg(symbol),
+  sqlc.arg(side),
+  sqlc.arg(price),
+  sqlc.arg(quantity),
+  sqlc.arg(quantity),
+  sqlc.arg(status),
+  sqlc.narg(idempotency_key),
+  sqlc.narg(canceled_at),
+  COALESCE(sqlc.narg(created_at), NOW()),
+  NOW()
+)
+RETURNING *;
 
+-- name: GetOrderByID :one
+SELECT *
+FROM orders
+WHERE id = $1;
+
+-- name: GetListOrdersByCustomerID :many
+SELECT *
+FROM orders
+WHERE customer_id = $1
+ORDER BY created_at DESC
+LIMIT $2 OFFSET $3;
+
+-- name: UpdateOrderStatus :exec
+UPDATE orders
+SET status = $2,
+    updated_at = NOW()
+WHERE id = $1;
